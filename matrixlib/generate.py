f9cc3babd09d0255d378098a026bb355a3af3c94
24io <tj.sd@outlook.com>
2024-06-02 13:24:50 +0200

    move background noise generation functions to matrixlib

--- matrixlib/generate.py
+++ matrixlib/generate.py
@@ -16,3 +16,32 @@ def __init_metadata(number_of_matrices: int, dimension: int) -> (np.ndarray, np.
     block_densities: np.ndarray = np.zeros(number_of_matrices, dtype=float)
     block_starts: np.ndarray = np.zeros((number_of_matrices, dimension), dtype=bool)
     return noise_densities, block_densities, block_starts
+
+
+def add_noise(
+        matrix_array: np.ndarray,
+        density_array: np.ndarray,
+        density_range: (float, float),
+        value_range: (float, float),
+) -> None:
+    # unpack and validate number of matrices and dimension
+    number_of_matrices, dimension, dimension_check = matrix_array.shape
+    if dimension_check != dimension:
+        raise ValueError("Dimension mismatch")
+
+    # unpack ranges
+    density_low: float = density_range[0]
+    density_high: float = density_range[1]
+    value_low: float = value_range[0]
+    value_high: float = value_range[1]
+
+    # create some random noise values (some might be overridden later)
+    for n in range(number_of_matrices):
+        noise_density: float = np.random.uniform(density_low, density_high)
+        density_array[n] = noise_density
+        for j in range(dimension):
+            for i in range(j):
+                if np.random.random() < noise_density:
+                    value: float = np.random.uniform(value_low, value_high)
+                    matrix_array[n][j][i] = value
+                    matrix_array[n][i][j] = value
