a438a9cd73879f4e81f5ad4fa1a110318bf41311
24io <tj.sd@outlook.com>
2024-06-06 17:16:13 +0200

    change block vector type to one-hot-encoded (float)

--- matrixlib/util.py
+++ matrixlib/util.py
@@ -6,7 +6,7 @@ def generate_block_vector_hex_string(block_vector: np.array) -> str:
     base = 1  # use this base 'counter' to prevent call of pow()
     int_rep = 0
     for e in block_vector:
-        if e:
+        if e != 0:
             int_rep += base
         base *= 2
     return f"{int_rep:016x}"
