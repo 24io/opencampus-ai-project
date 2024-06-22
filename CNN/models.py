6d8161aca8062fa26538bf683bdbfa1bed0a99c1
Anna-Valentina Hirsch <a-valentina.hirsch@hotmail.com>
2024-06-22 23:35:28 +0200

    fixed architecture to allow rebuilding model with saved weights

--- CNN/models.py
+++ CNN/models.py
@@ -1,69 +1,12 @@
 # Import necessary libraries
-import tensorflow as tf
-
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, Activation, BatchNormalization, ZeroPadding2D
+from tensorflow.keras.layers import *
 from tensorflow.keras.regularizers import l2
 
 
-## Baseline Model
-# class Baseline2(tf.keras.models.Model):
-#     def __init__(self, input_shape):
-#         super(Baseline, self).__init__()
-#         # First bottleneck unit
-#         self.bn1 = BatchNormalization()
-#         self.activation1 = Activation('selu')
-#         self.conv1 = Conv2D(32, kernel_size=(5, 5), padding='same', kernel_regularizer=l2(0.02))
-#         self.bn2 = BatchNormalization()
-#         self.activation2 = Activation('selu')
-#         self.conv2 = Conv2D(128, kernel_size=(3, 3), padding='same', kernel_regularizer=l2(0.02))
-#
-#         # Corner detection
-#         self.bn3 = BatchNormalization()
-#         self.padding = ZeroPadding2D(padding=(0, 3))
-#         self.conv3 = Conv2D(32, kernel_size=(21, 7), padding='valid', activation='tanh')
-#         self.conv4 = Conv2D(128, kernel_size=(1, 3), padding='same', activation='tanh')
-#
-#         # Fully-connected predictor
-#         self.flat = Flatten()
-#         self.classify = Dense(512, activation='sigmoid')
-#         self.dropout = Dropout(0.1)
-#         self.result = Dense(input_shape[1], activation='sigmoid')
-#
-#     def build(self, input_shape):
-#         inputs = Input(shape=input_shape[1:]) # input_shape[1:] = (height, width, channels)
-#         x = self.bn1(inputs)
-#         x = self.activation1(x)
-#         x = self.conv1(x)
-#         x = self.bn2(x)
-#         x = self.activation2(x)
-#         x = self.conv2(x)
-#
-#         merged = tf.keras.layers.add([inputs, x])
-#
-#         # Corner detection
-#         x = self.bn3(merged)
-#         x = self.padding(x)
-#         x = self.conv3(x)
-#         x = self.conv4(x)
-#
-#         # Fully-connected predictor
-#         x = self.flat(x)
-#         x = self.classify(x)
-#         x = self.dropout(x)
-#         x = self.result(x)
-#
-#         return tf.keras.models.Model(inputs=inputs, outputs=x)
-#
-#     def call(self, inputs, training=False):
-#         if not hasattr(self, 'model'):
-#             self.build(inputs.shape)
-#         return self.model(inputs)
-#
-
-
-class Baseline(tf.keras.models.Model):
+class Baseline(tf.keras.Model):
     def __init__(self, input_shape):
         super(Baseline, self).__init__()
+        self.input_shape = input_shape
 
         # First bottleneck unit
         self.bn1 = BatchNormalization()
@@ -112,4 +55,10 @@ class Baseline(tf.keras.models.Model):
 
         return x
 
-# TODO: fix model class to support keras model saving
+    def build(self, input_shape):
+        super(Baseline, self).build(input_shape)
+        self.call(tf.keras.layers.Input(shape=input_shape[1:]))
+
+    def model(self):
+        x = tf.keras.layers.Input(shape=self.input_shape)
+        return tf.keras.Model(inputs=[x], outputs=self.call(x))
